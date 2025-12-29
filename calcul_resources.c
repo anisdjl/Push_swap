3fd6419d3b4b35cf37925c80f1a7e13ce5d9f34b
anis <anis@MacBook-Air-de-anis.local>
2025-12-29 08:23:57 +0100

    add

--- calcul_resources.c
+++ calcul_resources.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calcul_resources.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 09:33:16 by adjelili          #+#    #+#             */
-/*   Updated: 2025/12/26 14:37:14 by adjelili         ###   ########.fr       */
+/*   Updated: 2025/12/28 14:21:28 by anis             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,39 @@ int	calcul_pos(dnode *stack, int pos)
 		return (size - pos);
 }
 
+int	search_pos_b(dnode *stack)
+{
+	
+}
+
+int calcul_at_a_given_postion(dnode *stack, int pos)
+{
+	
+}
+
+void	execute_rules(dnode **stack_a, dnode **stack_b, int cost_a, int cost_b)
+{
+	
+}
+
 int	calcul_cost(dnode *stack_a, dnode *stack_b)
 {
-	int	pos;
-	int	cost_a;
-	int	cost_b;
 	int	total;
+	int	best_pos;
 	
-	cost_a = calcul_pos
+	while (ft_lstsize(stack_a) > 3)
+	{
+		total = 
+	}
 }
+
+
+
+
+// j'envoie les stacks a et b à une fonction qui va vider la stack_a (sauf 3 elements) dans b
+// cette function calcule le cout total en calculant le cout de stack_a (c'est le nombre de ra ou de rra 
+// la postion de l'element dans a)
+// ensuite on cherche la target avec une fonction qui cherche la target puis calcule sa position et on obtient le calcul total)
+// une fois qu'on a calcule le plus opti et on l'envoie dans b
+// pour trouver le plus opti on fait une soustraction pour trouver les rr et les rrr 
+// r->(+) et rr->(-) donc on fait la soustraction que si c'est du meme signe
